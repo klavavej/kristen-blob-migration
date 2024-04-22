2d00c04647228f20228726d0f3732b628ebf525b
Kristen Lavavej <lavavej@Kristens-MacBook-Pro.local>
2024-04-22 13:03:49 -0700

    add test

---
+++ netlify/functions/test-set.ts
@@ -0,0 +1,11 @@
+import { getStore } from "@netlify/blobs";
+import type { Context } from "@netlify/functions";
+
+export default async (req: Request, context: Context) => {
+  const test = getStore("test");
+  await test.set("item 1", "description 1");
+  await test.set("item 2", "description 2");
+  await test.set("item 3", "description 3");
+
+
+  return new Response("test blobs set");
